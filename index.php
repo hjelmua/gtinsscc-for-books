3e93d23154214da39a0c8adcac14c5967c3557a4
hjelmua <jonas.hjelm@pentarex.se>
2025-01-31 19:53:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Generate Label PDF</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <style>
         body { background-color: #f8f9fa; }
         .container { max-width: 600px; margin-top: 50px; }
